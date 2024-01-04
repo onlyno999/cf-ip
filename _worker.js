4608e7242d0fe016023907bc2cd33d40bf4d5c95
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-01-04 23:38:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'mirage-massive-shingle.glitch.me'
+      url.hostname = '2001:67c:2960:6464::6823:cae8'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
