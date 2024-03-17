e85df87f642336891018318b242ef33f54552914
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-03-17 10:57:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'mirage-massive-shingle.glitch.me'
+      url.hostname = 'sb.box9.dedyn.io/config/https://go.onlyno999.dynv6.net4bb5-bf3d-c1fa19afd98c'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
