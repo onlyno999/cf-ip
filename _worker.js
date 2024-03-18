144a5b48648f9f08d85102e76560fecfdc353ccd
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-03-18 16:28:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'go.onlyno999.dynv6.net4bb5-bf3d-c1fa19afd98c'
+      url.hostname = 'yamcode.com/raw/clash-meta'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
