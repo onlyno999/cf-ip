d45587252d27f2c7f0dc7cb48e1445e52335b2db
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-03-20 03:35:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.visa.com.hk'
+      url.hostname = 'www.visa.com.hk/zh_HK'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
